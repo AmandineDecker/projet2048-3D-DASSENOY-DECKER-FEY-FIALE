ba3662cd857a797bbb8426edf18e632154322cff
ValentineDssn <valentine.dassenoy@gmail.com>
2019-10-05 17:30:26 +0200

    Modification de la fonction toucheclavier, ajout des deux premières tuiles de la partie au hasard

--- src/application/FXMLDocumentController.java
+++ src/application/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package application;
 
 import java.net.URL;
+import java.util.HashSet;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,6 +21,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import model.Case;
+import model.Grille;
 
 /**
  *
@@ -44,8 +47,49 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label score, logo, commandes, resultat;
     
+    private Grille modelGrille = new Grille();
+    private HashSet<Case> grille;
     
-    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        System.out.println("le contrôleur initialise la vue");
+        fond.getStyleClass().add("pane");
+        gr1.getStyleClass().add("gridpane");
+        
+        //Initialisation de la partie avec les deux premières cases aux hasard 
+        boolean b = modelGrille.nouvelleCase();
+        b = modelGrille.nouvelleCase();
+        grille = modelGrille.getGr();
+        System.out.println(grille);
+        for (Case c : grille){
+          Pane p = new Pane ();
+          p.getStyleClass().add("tuile");
+          Label l = new Label(String.valueOf(c.getVal()));
+          l.getStyleClass().add("valeurTuile");
+          p.getChildren().add(l);    
+          switch (c.getZ()){
+              case 0:
+                  gr1.add(p, c.getX(), c.getY());
+                  break;
+              case 1:
+                  gr2.add(p, c.getX(), c.getY());
+                  break;
+             case 2:
+                  gr3.add(p, c.getX(), c.getY());
+                  break;
+                  
+          }
+          p.setVisible(true);
+          l.setVisible(true);
+          
+        }
+        
+        
+    }
+       
+    
+     @FXML
     public void quitter(ActionEvent event) {
         System.exit(0);
     }
@@ -55,19 +99,28 @@ public class FXMLDocumentController implements Initializable {
         System.out.println("You clicked me!");
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        System.out.println("le contrôleur initialise la vue");
-        fond.getStyleClass().add("pane");
-        gr1.getStyleClass().add("gridpane");
-
-    }    
-    
-    
     @FXML
     public void toucheClavier(KeyEvent ke) {
-        System.out.println("touche clavier appuyée");
+        switch(ke.getText()){
+            case "z" :
+                System.out.println("Vous vous déplacez vers le haut");
+                break;
+            case "q":
+                System.out.println("Vous vous déplacez vers la gauche");
+                break;
+             case "s":
+                System.out.println("Vous vous déplacez vers le bas");
+                break;
+             case "d":
+                System.out.println("Vous vous déplacez vers la droite");
+                break; 
+             case "r":
+                System.out.println("Vous vous déplacez sur la grille supérieure");
+                break;
+            case "f":
+                System.out.println("Vous vous déplacez sur la grille inférieur");
+                break;
+        }
     }
     
 }
